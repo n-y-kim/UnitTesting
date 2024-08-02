53617cd6243d098ecb65731bd273bf4e5a9211a4
n-y-kim <nyn2265@gmail.com>
2024-08-02 23:38:58 +0900

    Init commit

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(candidate); i++)
+            {
+                if (candidate % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
